c23fe59db2333010f0492b9980d5561a2643c869
Alhassan Abubakar Sadiq <bnsadiq82@gmail.com>
2026-05-17 16:07:14 +0100

    Rebuild web: 4 mood modes and responsive design

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "c57e9ed941529a0b85e939227a2d37f6",
+const RESOURCES = {"flutter_bootstrap.js": "cd5add0b345ca13d053a7efad5cab9c0",
 "version.json": "d475322440584b8ee2e4f7f534d58159",
 "index.html": "fa6e9c209fe8211cee14cb425b93686f",
 "/": "fa6e9c209fe8211cee14cb425b93686f",
-"main.dart.js": "7ddfb613ec644ae099bd7bf17b609303",
+"main.dart.js": "18c44cf5f339d549ef6e891ed61ca1cd",
 "flutter.js": "24bc71911b75b5f8135c949e27a2984e",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
@@ -15,7 +15,7 @@ const RESOURCES = {"flutter_bootstrap.js": "c57e9ed941529a0b85e939227a2d37f6",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "manifest.json": "579da6a8042a54a5a333651bc76d9548",
-"assets/NOTICES": "d5fa2985b5c262367009cb7bfd24d1d1",
+"assets/NOTICES": "d7106a1e699ac2b3ecab38b5189d08b1",
 "assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
 "assets/AssetManifest.bin.json": "69a99f98c8b1fb8111c5fb961769fcd8",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "33b7d9392238c04c131b6ce224e13711",
